5d24a6353cd04b47c5ac2c585fdda124034f667a
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-04 23:00:55 +0000

    [feat]: removed var keywords to avoid declaration

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,9 +2,10 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  // removed var keywords to avoid declaration
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+   task = true;
+   task2 = false;
   }
   return [task, task2];
 }
