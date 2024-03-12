292c89cbe931ede14223b1aebd9810459ba651ad
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-12 23:50:26 +0000

    [feat]: typed arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+function createInt8TypedArray(length, position, value) {
+  if (position >= length || position < 0) {
+    throw new Error('Position outisde range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+
+  return view;
+}
+export default createInt8TypedArray;
