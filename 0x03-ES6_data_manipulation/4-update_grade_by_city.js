9212d240f4f139d345d4e1749c20d9e3607e9149
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-12 23:43:40 +0000

    [feat]: Implementation of updateStudentGradeByCity function

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchedGrade = newGrades.find((grade) => grade.studentId === student.id);
+      if (matchedGrade) {
+        return { ...student, grade: matchedGrade.grade };
+      }
+      return { ...student, grade: 'N/A' };
+    });
+}
+export default updateStudentGradeByCity;
