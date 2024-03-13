c54114fc227f24ba669120edd269d3d231193725
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-13 00:16:09 +0000

    [feat]: clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+function cleanSet(set, startString) {
+  return [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+}
+export default cleanSet;
