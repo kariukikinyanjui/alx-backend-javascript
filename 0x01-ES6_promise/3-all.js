a8a71dcb2e807a9ec9d2934d1f2b697b565b5bfd
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-17 08:22:15 +0000

    [feat]: handled multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
-    })
-    .catch(() => {
-      console.error('Signup system offline');
-    });
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  return Promise.all([photo, user]).then((res) => {
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
-export default handleProfileSignup;
