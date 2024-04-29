addc7c76ec888e4e82a4d3c9b8b20de314d965d1
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-04-29 14:20:57 +0000

    [feat]: reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+function countStudents (path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.trim().split('\n').filter(line => line.trim() !== '');
+
+    // Initialize counters for each field
+    let csCount = 0;
+    let sweCount = 0;
+    const csStudents = [];
+    const sweStudents = [];
+
+    // Loopo through each line and count students in each field
+    for (const line of lines) {
+      const [, , , field] = line.split(',');
+      if (field === 'CS') {
+        csCount++;
+        csStudents.push(line.split(',')[0]);
+      } else if (field === 'SWE') {
+          sweCount++;
+          sweStudents.push(line.split(',')[0]);
+      }
+    }
+    // Log the total number of students
+    console.log(`Number of students: ${lines.length}`);
+
+    // Log the number of students in each field along with their names
+    console.log(`Number of students in CS: ${csCount}. LIst: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweCount}. List: ${sweStudents.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
