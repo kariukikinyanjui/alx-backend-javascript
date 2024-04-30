6c808cb744c86a7ed1b2617fed9c184a48c7f794
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-04-30 00:39:15 +0000

    [feat]: reading a file synchronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,36 +1,38 @@
 const fs = require('fs');
 
-function countStudents (path) {
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.trim().split('\n').filter(line => line.trim() !== '');
-
-    // Initialize counters for each field
-    let csCount = 0;
-    let sweCount = 0;
-    const csStudents = [];
-    const sweStudents = [];
-
-    // Loopo through each line and count students in each field
-    for (const line of lines) {
-      const [, , , field] = line.split(',');
-      if (field === 'CS') {
-        csCount++;
-        csStudents.push(line.split(',')[0]);
-      } else if (field === 'SWE') {
-        sweCount++;
-        sweStudents.push(line.split(',')[0]);
-      }
-    }
-    // Log the total number of students
-    console.log(`Number of students: ${lines.length}`);
-
-    // Log the number of students in each field along with their names
-    console.log(`Number of students in CS: ${csCount}. LIst: ${csStudents.join(', ')}`);
-    console.log(`Number of students in SWE: ${sweCount}. List: ${sweStudents.join(', ')}`);
-  } catch (error) {
-    throw new Error('Cannot load the database');
+const processStudents = (filePath) => {
+  if (!fs.existsSync(filePath)) {
+    throw new Error('Could not locate student data file.');
   }
-}
+  if (!fs.statSync(filePath).isFile()) {
+    throw new Error('Student data file is not a valid file.');
+  }
 
-module.exports = countStudents;
+  const fileContents = fs.readFileSync(filePath, 'utf-8');
+  const dataLines = fileContents.trim().split('\n');
+
+  const studentByField = {};
+  const fieldHeaders = dataLines[0].split(',');
+  const studentProperties = fieldHeaders.slice(0, fieldHeaders.length - 1);
+
+  for (const line of dataLines.slice(1)) {
+    const studentData = line.split(',');
+    const studentValues = studentData.slice(0, studentData.length - 1);
+    const program = studentData[studentData.length - 1];
+    if (!Object.keys(studentByField).includes(program)) {
+      studentByField[program] = [];
+    }
+    const studentEntry = studentProperties.map((prop, idx) => [prop, studentValues[idx]]);
+    studentByField[program].push(Object.fromEntries(studentEntry));
+  }
+
+  const totalStudents = Object.values(studentByField).reduce((prev, curr) => prev + curr.length, 0);
+  console.log(`Number of students: ${totalStudents}`);
+
+  for (const [program, students] of Object.entries(studentByField)) {
+    const studentNames = students.map((student) => student.firstname).join(', ');
+    console.log(`Number of students in ${program}: ${students.length}. List: ${studentNames}`);
+  }
+};
+
+module.exports = processStudents;
