1c3147bf8eddb3a21f544e06e6a3ddeb99af0437
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-04-30 17:20:03 +0000

    [feat]: a small HTTP server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,15 @@
 const http = require('http');
 
 // Create HTTP server
+const PORT = 1245;
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   res.end('Hello Hoberton School!');
 });
 
-app.listen(1245);
+app.listen(PORT, '127.0.0.1', () => {
+  console.log('Server running');
+});
 
 module.exports = app;
