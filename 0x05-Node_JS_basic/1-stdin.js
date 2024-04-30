d400dbbc7a27e53dea03d600149a88939d59d252
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-04-30 00:15:26 +0000

    [feat]: added comments

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,17 @@
+// Welcome message and prompt user name
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+// Sets up a listener for the readable event
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
 
+  // Check if name has a value(not null)
   if (name) {
     process.stdout.write(`Your name is: ${name}`);
   }
 });
 
+// Sets up a listener for the end event
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
