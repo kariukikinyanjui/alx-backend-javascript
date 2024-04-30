1769b955456ba74cf3adae5b4ffa1ce7681326c2
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-04-30 23:45:02 +0000

    [feat]: a complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,32 +1,35 @@
 const http = require('http');
+const { URL } = require('url');
 const countStudents = require('./3-read_file_async');
-const PORT = 1245;
 
-const app = http.createServer(async (req, res) => {
-  const { pathname } = new URL(req.url, 'http://localhost:1245');
+const app = http.createServer((req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
 
-  if (pathname === '/') {
+  if (url.pathname === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-  } else if (pathname === '/students') {
-    const databaseFile = req.headers['x-filename'] || 'database.csv';
-
-    try {
-      const studentData = await countStudents(databaseFile);
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end(studentData);
-    } catch (error) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Error: Cannot load the database');
-    }
+  } else if (url.pathname === '/students') {
+    const dbPath = process.argv[2];
+    countStudents(dbPath)
+      .then(() => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write('This is the list of our students');
+        res.end();
+      })
+      .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.write(error.message);
+        res.end();
+      });
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('404 Not Found');
   }
 });
 
+const PORT = 1245;
 app.listen(PORT, () => {
-  console.log(`Sever running on port ${PORT}/`);
+  console.log(`Server running at port ${PORT}`);
 });
 
 module.exports = app;
