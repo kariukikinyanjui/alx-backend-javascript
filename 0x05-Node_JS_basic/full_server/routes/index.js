d6fd7f72b65b6156d89b626731076f076383dab9
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-05-01 01:29:08 +0000

    [feat]: updated the request object to include the file path

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -8,7 +8,14 @@ const router = express.Router();
 router.get('/', AppController.getHomepage);
 
 // Routes for students
-router.get('/students', StudentsController.getAllStudents);
-router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+router.get('/students', (req, res) => {
+  req.filePath = process.argv[2]; // Set the file path here
+  StudentsController.getAllStudents(req, res);
+});
+
+router.get('/students/:major', (req, res) => {
+  req.filePath = process.argv[2]; // Set the file path here
+  StudentsController.getAllStudentsByMajor(req, res);
+});
 
 export default router;
