08e4eb7406a9326ce0ea1d192062f244f10210ed
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-05-02 09:04:38 +0000

    [feat]: using spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendpaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function() {
+  it('should call Utils.calculateNumber with correct arguments', function() {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    calculateNumberSpy.restore();
+  });
+});
