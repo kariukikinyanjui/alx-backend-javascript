aec9f083dc54942553ae12b4434167f31430cc84
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-05-02 08:19:53 +0000

    [feat]: basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,18 @@
+function calculateNumber(type, a, b) {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  } else if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  } else if (type === 'DIVIDE') {
+    const roundedB = Math.round(b);
+    if (roundedB === 0) {
+      return 'Error';
+    } else {
+      return Math.round(a) / roundedB;
+    }
+  } else {
+    return 'Invalid type';
+  }
+}
+
+module.exports = calculateNumber;
