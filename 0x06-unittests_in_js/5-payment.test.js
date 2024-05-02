8ceab88c58f0a1b323deec08b6f229da050f1e61
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-05-02 12:57:24 +0000

    [feat]: using hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function() {
+  let consoleSpy;
+
+  beforeEach(function() {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function() {
+    console.Spy.restore();
+  });
+
+  it('should log the correct message for totalAmount 100 and totalShipping 20', function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+  });
+
+  it('should log the correct message for totalAmount 10 and totalShipping 10', function() {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+  });
+});
