70b87a2a15477cf4db9df8e7ca1409937b6a3faa
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-05-03 01:24:23 +0000

    [feat]: regex integration testing

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,31 +1,15 @@
 const express = require('express');
-
 const app = express();
+
 const PORT = 7865;
 
-app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
 
-const paymentMethods = {
-  12: ['paypal', 'stripe'],
-  15: ['paypal', 'visa'],
-  18: ['stripe', 'maestro']
-};
-
-// Middleware to validate :id as a number
-const validateId = (req, res, next) => {
+app.get('/cart/:id(\\d+)', (req, res) => {
   const { id } = req.params;
-  if (!/^\d+$/.test(id)) {
-    return res.status(404).send('Not Found');
-  }
-  next();
-};
-
-app.get('/cart/:id', validateId, (req, res) => {
-  const { id } = req.params;
-  if (!paymentMethods[id]) {
-    return res.status(404).send('Cart not found');
-  }
-  res.send(`Payment methods for cart ${id}: ${paymentMethods[id].join(', ')}`);
+  res.send(`Payment methods for cart ${id}`);
 });
 
 app.listen(PORT, () => {
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,16 +1,27 @@
-const request = require('supertest');
+const { expect } = require('chai');
+const request = require('request');
 const app = require('./api');
 
-describe('GET /cart/:id', () => {
-  it('should return payment methods for cart with correct status code when :id is a number', async () => {
-    const response = await request(app).get('/cart/12');
-    expect(response.status).toEqual(200);
-    expect(response.text).toEqual('Payment methods for cart 12: paypal, stripe');
+describe('Cart page', function() {
+  it('should return status code 200 when :id is a number', function(done) {
+    request.get('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
   });
 
-  it('should return 404 status code when :id is NOT a number', async () => {
-    const response = await request(app).get('/cart/hello');
-    expect(response.status).toEqual(404);
+  it('should return correct result when :id is a number', function(done) {
+    request.get('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return status code 404 when :id is NOT a number', function(done) {
+    request.get('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
   });
 });
 
