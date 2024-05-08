ea9848014ca406262768df058dee4fe1ce355432
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-05-08 20:46:33 +0000

    [feat]: basic test using Mocha and Node assertion

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,19 +2,11 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function() {
-  it('should return the sum of rounded a and b', function() {
+  it('should return the correct sum of rounded numbers', function() {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-
-  it('should work with negative numbers', function() {
-    assert.strictEqual(calculateNumber(-1.2, -3.7), -5);
-    assert.strictEqual(calculateNumber(-1.5, -3.7), -6);
-  });
-
-  it('should work with one of the numbers being 0', function() {
-    assert.strictEqual(calculateNumber(0, 3.7), 4);
-    assert.strictEqual(calculateNumber(1.2, 0), 1);
   });
 });
 
